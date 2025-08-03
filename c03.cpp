444c12e8754dbb8f08889fc18838b3896f2fec73
Geekunknown29 <123154889+Geekunknown29@users.noreply.github.com>
2025-08-03 19:31:19 +0530

    Update c03.cpp

--- c03.cpp
+++ c03.cpp
@@ -1,1 +1,55 @@
+/*125. Valid Palindrome
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ */
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        for (int i = 0 ; i< s.size() ; i++){
+            if(!isalnum(s[i])){
+                s.erase(i,1);
+                i--;
+            }
+            
+        }
+       
+        cout << s << endl;
+        string rev(s.rbegin(), s.rend()) ;
+
+
+         for(int i = 0 ; i <s.size() ; i++){
+            s[i] = tolower(s[i]) ;
+            rev[i] = tolower(rev[i]) ;
+         }
+
+        if(s == rev){
+            return true ;
+
+        }
+        else{
+            return false;
+        }
+
+    }
+};
 
